8cd2b979c9db3b0fc96390229fb3f5463fc8fb6e
Covina <jabs303@gmail.com>
2017-04-18 23:10:20 -0700

    Section 4, Lecture 40;  Creating prefabs for enemies; Created checkpoints, adjusted colliders;

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+	public static GameManager instance = null;
+
+	// hold our spawn point
+	public GameObject spawnPoint;
+
+	// hold our skeletons
+	public GameObject[] enemies;
+
+	// enemy counter for increasing waves
+	public int maxEnemiesOnScreen;
+
+	// enemy count in the wave
+	public int totalEnemies;
+
+	// how many get spawned at a single time
+	public int enemiesPerSpawn;
+
+
+	// keep track of enemies on the screen
+	private int currentEnemiesOnScreen = 0;
+	
+
+	void Awake ()
+	{
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this){
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void spawnEnemy ()
+	{
+
+	}
+}
