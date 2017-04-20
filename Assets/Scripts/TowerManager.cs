09a86cfbf294dda8366010105b532a568ea25195
Covina <jabs303@gmail.com>
2017-04-19 22:52:55 -0700

    Section 4, Lecture 44;  Tower Placements on Build Sites

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -24,14 +24,16 @@ public class TowerManager : Singleton<TowerManager> {
 
 
 			// store the x,y of the mouse click
-			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 
 			// fire the ray at the mouse click (world point), going in no direction
-			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+			RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero);
 
-			//Debug.Log("Update() :: Mouse Clicked at " + worldPoint);
+			// only place towers on build site locations
+			if (hit.collider.tag == "BuildSite") {
 
-			PlaceTower(hit);
+				PlaceTower (hit);
+			}
 
 		}
 
@@ -41,36 +43,16 @@ public class TowerManager : Singleton<TowerManager> {
 	public void SelectTower (TowerButton towerSelected)
 	{
 
-		//Debug.Log("SelectTower() called;  Parameter passed: " + towerSelected);
 		towerButtonPressed = towerSelected;
-
-		//Debug.Log("SelectTower() ::  towerButtonPressed.gameObject.name: " + towerButtonPressed.gameObject.name);
-
-		//Debug.Log("SelectTower() ::  towerButtonPressed.TowerObject : " + towerButtonPressed.TowerObject);
 
 	}
 
 	public void PlaceTower (RaycastHit2D hit)
 	{
 
-		//Debug.Log ("PlaceTower() called");
-
-//		if (EventSystem.current.IsPointerOverGameObject ()) {
-//			Debug.Log ("PlaceTower() - EventSystem.current.IsPointerOverGameObject is TRUE :: " + EventSystem.current.IsPointerOverGameObject() );
-//		}
-//
-//		if (towerButtonPressed == null) {
-//			Debug.Log ("PlaceTower() - towerButtonPressed IS NULL :: " + towerButtonPressed); 
-//		}
 
 		// check for UI and make sure a tower is selected
 		if (!EventSystem.current.IsPointerOverGameObject() && towerButtonPressed != null) {
-
-
-//			Debug.Log("PlaceTower() - towerButtonPressed :: " + towerButtonPressed);
-//			Debug.Log("PlaceTower() - towerButtonPressed.gameObject.name :: " + towerButtonPressed.gameObject.name);
-//			Debug.Log("PlaceTower() - towerButtonPressed.TowerObject :: " + towerButtonPressed.TowerObject);
-//			Debug.Log("PlaceTower() - towerButtonPressed.TowerObject.gameObject.name :: " + towerButtonPressed.TowerObject.gameObject.name);
 
 
 			// instantiate the tower
